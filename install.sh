483df1716938bdc0bab46db76611474fd043d12f
MarcDorval <marc.dorval@silabs.com>
2018-11-12 15:57:54 +0000

    install script renamed

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_FOLDER="/home/pi/siliconlabs/wfx_tools/linux_scripts/"
+PWD=$(pwd)
+cd ${SCRIPT_FOLDER}
+
+all_valid_scripts=$( find -type f | grep wfx_ )
+
+sudo rm -f /usr/local/bin/wfx_*
+
+# Create a link under /usr/local/bin for all files matching wfx_ and not containing '.'
+for script in ${all_valid_scripts}; do 
+	script_realpath=$( realpath ${script} )
+	basename=$( basename ${script} )
+	dot_test=$( echo {$basename} | grep "." )
+	if [ "${dot_test}" != "" ]; then
+		sudo ln -fs ${script_realpath} /usr/local/bin/${basename}
+	fi
+done
+
+ls -l /usr/local/bin/* --color=auto
+
+cd ${PWD}
