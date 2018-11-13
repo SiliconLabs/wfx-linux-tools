b254f2b8057b4f78a6e82aa3eb94835767ea2014
Xavier Langellier <xavier.langelier@silabs.com>
2018-11-13 17:21:53 +0000

    Improve AP start script

--- demos/demo_ap.sh
+++ demos/demo_ap.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -ex
 
-# Start an access point
+# Configure WLAN interface for access point mode
 
 . wfx_set_env
 check_root
 
-check_no_process wpa_supplicant
-check_no_process hostapd
-check_no_process dhcpcd
-
 INTERFACE="wlan0"
 ADDRESS="192.168.51.1/24"
+DNSMASQ_CONF=$SILABS_ROOT/wfx_tools/demos/conf/dnsmasq.conf
+HOSTAPD_CONF=$SILABS_ROOT/wfx_tools/demos/conf/hostapd.conf
 
-# TODO: check if wpa_supplicant or hostapd or dnsmasq need to be stopped
+# Check wlan0
+if ! ip link show "$INTERFACE" &> /dev/null; then
+    >&2 echo "Interface $INTERFACE not detected, exiting"
+    exit 1
+fi
 
-# Tell dhcpcd to release interface
+# Kill potentially started process
+kill_check wpa_supplicant hostapd dnsmasq
+
+# Tell dhcpcd to release WLAN interface
 dhcpcd --release "$INTERFACE"
 
 # Set static IP configuration
@@ -23,10 +28,10 @@ ip addr add "$ADDRESS" dev "$INTERFACE"
 ip link set "$INTERFACE" up
 
 # Start DHCP server
-dnsmasq -C conf/dnsmasq.conf
+dnsmasq -C "$DNSMASQ_CONF"
 
 # Start hostapd
-hostapd -B conf/hostapd.conf
+hostapd -B "$HOSTAPD_CONF"
 
 # Enable ip forward and masquerading
 echo 1 > /proc/sys/net/ipv4/ip_forward
