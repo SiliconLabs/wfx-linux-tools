99d081591eac367d02ea1ef6badec66eaf4eb614
Xavier Langellier <xavier.langelier@silabs.com>
2018-11-13 13:14:52 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@
 set -e
 
 [ $(id -u) != 0 ] && echo "Please run this script as root (running 'sudo $0' should work)" && exit 1 || true
-![ -e install.sh ] && echo "This script must be run from wfx_tools" && exit 1 || true
+! [ -e install.sh ] && echo "This script must be run from wfx_tools" && exit 1 || true
 rm -f /usr/local/bin/wfx_*
 rm -f /usr/local/bin/pds_compress
 
 # Create a link under /usr/local/bin for all files matching wfx_ and not containing '.'
 # TODO: add pds_compress
 for f in $(find -type f -name "wfx_*" -perm -100); do
-    if [[ $f != *.* && $f != *~ ]]; then
-	ln -fsn $(realpath ${f}) /usr/local/bin/
+    b=$(basename $f)
+    if [[ $b != *.* && $b != *~ ]]; then
+	ln -s $(realpath ${f}) /usr/local/bin/$b
     fi
 done
 
-dtc dt-overlays/wfx-spi-overlay.dts >  /boot/overlays/wfx-spi.dtbo
-dtc dt-overlays/wfx-sdio-overlay.dts >  /boot/overlays/wfx-sdio.dtbo
+dtc linux_overlays/wfx-spi-overlay.dts > /boot/overlays/wfx-spi.dtbo
+dtc linux_overlays/wfx-sdio-overlay.dts > /boot/overlays/wfx-sdio.dtbo
