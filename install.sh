f45b892c6d0a8d049cd85b2a9ecf3d1db4c3f1fa
Marc Dorval <marc.dorval@silabs.com>
2018-11-14 09:13:26 +0100

    Removing TODO from install script

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ rm -f /usr/local/bin/wfx_*
 rm -f /usr/local/bin/pds_compress
 
 # Create a link under /usr/local/bin for all files matching wfx_ and not containing '.'
-# TODO: add pds_compress
 for f in $(find -type f -name "wfx_*" -perm -100) pds_compress; do
     b=$(basename $f)
     if [[ $b != *.* && $b != *~ ]]; then
