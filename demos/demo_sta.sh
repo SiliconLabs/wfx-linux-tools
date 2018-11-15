bbdc7f0a3d2e64ccefd29872df27a56d6a567b39
Xavier Langellier <xavier.langelier@silabs.com>
2018-11-15 14:07:23 +0000

    Fix demo_sta.sh and add wpa_supplicant.conf

--- demos/demo_sta.sh
+++ demos/demo_sta.sh
@@ -3,10 +3,9 @@
 # Start a demo in station mode for browsing
 
 . wfx_set_env
-check_root
+check_no_root
 
 INTERFACE="wlan0"
-# TODO: write wpa_supplicant.conf
 WPA_SUPPLICANT_CONF=$SILABS_ROOT/wfx_tools/demos/conf/wpa_supplicant.conf
 
 # Check wlan0
@@ -15,15 +14,18 @@ if ! ip link show "$INTERFACE" &> /dev/null; then
     exit 1
 fi
 
+# Try to stop a potentially running wpa_supplicant (then wait for termination)
+wpa_cli -i "$INTERFACE" terminate 2>/dev/null && sleep 1 || true
+
 # Kill potentially started process
-kill_check wpa_supplicant hostapd dnsmasq
+sudo bash -c '. wfx_set_env; kill_check wpa_supplicant hostapd dnsmasq wpa_gui'
+
+# Tell dhcpcd to control WLAN interface (in case of previous demo_AP)
+sudo dhcpcd --rebind "$INTERFACE"
 
 # Start wpa_supplicant
-wpa_supplicant -i "$INTERFACE" -c "$WPA_SUPPLICANT_CONF" -B
-
-# Tell dhcpcd to control WLAN interface
-dhcpcd --rebind "$INTERFACE"
+sudo wpa_supplicant -i "$INTERFACE" -c "$WPA_SUPPLICANT_CONF" -B -s
 
 # TODO: Start wpagui
+wpa_gui &
 # TODO: check that we have a screen
-# TODO: check if we need to disable lxpanel (on generated image)
