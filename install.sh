a47e6470269913e618942b09776bc985c4d6163f
Xavier Langellier <xavier.langelier@silabs.com>
2018-12-05 17:16:49 +0100

    Rename repositories

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 set -e
 
 [ $(id -u) != 0 ] && echo "Please run this script as root (running 'sudo $0' should work)" && exit 1 || true
-! [ -e install.sh ] && echo "This script must be run from wfx_tools" && exit 1 || true
+! [ -e install.sh ] && echo "This script must be run from wfx-linux-tools" && exit 1 || true
 rm -f /usr/local/bin/wfx_*
 rm -f /usr/local/bin/pds_compress
 
