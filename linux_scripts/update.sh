6ae780d9898f49335c75c0006c6fcb8698162788
Xavier Langellier <xavier.langelier@silabs.com>
2018-12-17 14:09:54 +0000

    Fix wfx-linux-tools path in update.sh

--- linux_scripts/update.sh
+++ linux_scripts/update.sh
@@ -8,7 +8,7 @@ set -e
 . wfx_set_env
 check_no_root
 
-"$GITHUB_TOOLS_PATH/wfx-linux-tools/linux_scripts/wfx_tools_install" 0.2
+"$GITHUB_TOOLS_PATH/linux_scripts/wfx_tools_install" 0.2
 wfx_driver_install 1.6-public
 sudo wfx_firmware_install FW1.2.15
 sudo wfx_pds_install BRD8022A_Rev_A05.pds.in
