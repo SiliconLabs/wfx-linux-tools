a693d7922c358b6b4906821b6f69be9ac8311586
Xavier Langellier <xavier.langelier@silabs.com>
2018-12-18 14:44:07 +0000

    Remove dependencies in update.sh and cleanup

--- update/tools_install.sh
+++ update/tools_install.sh
@@ -2,20 +2,21 @@
 # Copyright (c) 2018, Silicon Laboratories
 # See license terms contained in COPYING file
 
+# DO NOT MOVE THIS SCRIPT OR MAKE IT EXECUTABLE
+# this script is to be run by wfx_tools_install
+
 set -ex
 
-GITHUB_TOOLS_PATH="/home/pi/siliconlabs/wfx-linux-tools"
 GITHUB_TOOLS_INTERNAL_PATH="$GITHUB_TOOLS_PATH/internal"
+GIT="git -C $GITHUB_TOOLS_PATH"
 
-VERSION=$1
-
-if [ -z "$VERSION" ]; then
+if [ -z "$TOOLS_VERSION" ]; then
     echo "ERROR: version needed" >&2
     exit 1
 fi
 
 if [ $# -gt 0 ]; then
-    STATUS=$(git -C $GITHUB_TOOLS_PATH status --porcelain --untracked-files=no)
+    STATUS=$($GIT status --porcelain --untracked-files=no)
     if ! [ -z "$STATUS" ]; then
 	echo "ERROR: the following files where modified in the direcory $REPO_PATH"
 	echo "$STATUS"
@@ -24,10 +25,15 @@ if [ $# -gt 0 ]; then
 	exit 1
     fi
 
-    if ! git -C $GITHUB_TOOLS_PATH checkout $1; then
-        echo "ERROR: cannot get version $1" >&2
+    # Fetch if version is unknown
+    if ! $GIT rev-parse --verify $TOOLS_VERSION &>/dev/null; then
+	$GIT fetch --all --tags --prune
+    fi
+
+    if ! $GIT checkout $TOOLS_VERSION; then
+        echo "ERROR: cannot get version $TOOLS_VERSION" >&2
         echo "Possible tags:"
-        git -C $GITHUB_TOOLS_PATH tag | sed 's/^/  /'
+        $GIT tag | sed 's/^/  /'
         exit 1
     fi
 fi
--- update/update.sh
+++ update/update.sh
@@ -6,12 +6,18 @@
 
 set -e
 
-. wfx_set_env
-check_no_root
+if [ $(id -u) == 0 ]; then
+    echo "ERROR: running this script as root is not recommended" >&2
+    exit 1
+fi
+
+export GITHUB_TOOLS_PATH="/home/pi/siliconlabs/wfx-linux-tools"
 
 set -x
 
-"$GITHUB_TOOLS_PATH/update/wfx_tools_install" 0.2
+"$GITHUB_TOOLS_PATH/update/wfx_tools_install" 0.4
+
+wfx_fetch
 wfx_driver_install 1.6-public
 sudo wfx_firmware_install FW1.2.15
 sudo wfx_pds_install BRD8022A_Rev_A05.pds.in
