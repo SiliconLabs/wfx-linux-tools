d3397c4974ce2d303efb225966f8484554aff0b3
Xavier Langellier <xavier.langelier@silabs.com>
2018-12-27 14:29:31 +0100

    Dummy commit to test update

--- update/update.sh
+++ update/update.sh
@@ -13,7 +13,7 @@ fi
 
 set -x
 
-"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 0.11
+"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 0.12
 
 wfx_fetch
 wfx_driver_install 1.6-public
