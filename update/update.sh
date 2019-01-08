e2a4852a7f2cc079901387dc8aa80441a7ffacf5
Xavier Langellier <xavier.langelier@silabs.com>
2019-01-08 16:10:42 +0000

    Update tools to 0.16

--- update/update.sh
+++ update/update.sh
@@ -13,7 +13,7 @@ fi
 
 set -x
 
-"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 0.15
+"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 0.16
 
 wfx_fetch
 wfx_driver_install 1.6-public
