a03849d495d3104154f3ee08e08c6db49f286a0d
Xavier Langellier <xavier.langelier@silabs.com>
2019-01-11 15:14:36 +0000

    Fix install.sh ln

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ rm -f /usr/local/bin/wfx_*
 rm -f /usr/local/bin/pds_compress
 
 # Create a link under /usr/local/bin for all files matching wfx_ (ignore hidden and backup files)
-find -type f \( -name 'wfx_*' -o -name pds_compress \) ! -name '*~' ! -name '.*' -execdir ln -vs $(realpath {}) /usr/local/bin/{} \;
+find -type f \( -name 'wfx_*' -o -name pds_compress \) ! -name '*~' ! -name '.*' -execdir bash -c 'ln -vs $(realpath {}) /usr/local/bin/$(basename {})' \;
 
 dtc -@ -W no-unit_address_vs_reg overlays/wfx-spi-overlay.dts -o /boot/overlays/wfx-spi.dtbo
 dtc -@ -W no-unit_address_vs_reg overlays/wfx-sdio-overlay.dts -o /boot/overlays/wfx-sdio.dtbo
