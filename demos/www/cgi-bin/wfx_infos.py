4aa8c2eb903e931614243dd6f442a45720340974
Xavier Langellier <xavier.langelier@silabs.com>
2019-01-11 08:18:49 +0000

    Fix wfx_infos.py spaces

--- demos/www/cgi-bin/wfx_infos.py
+++ demos/www/cgi-bin/wfx_infos.py
@@ -24,54 +24,54 @@ print ("Content-type:text/html\r\n\r\n")
 
 print ("""
 <!DOCTYPE html>
-        <html lang="en">
-          <head>
-                <meta charset="utf-8">
-                <title>Silicon Labs WFX</title>
-                <link rel="stylesheet" href="/silabs.css" type="text/css">
-          </head>
+	<html lang="en">
+	  <head>
+		<meta charset="utf-8">
+		<title>Silicon Labs WFX</title>
+		<link rel="stylesheet" href="/silabs.css" type="text/css">
+	  </head>
 """)
 
 print ("""
   <body>
-        <div class="column">
-                <div class="row">
-                        <br>
-                        <div class="column">
-                                <div class="row">
-                                        <hr>
-                                        <table>
-                                                <tr>
-                                                        <td>
-                                                                <div class="logo">
-                                                                        <a href="//www.silabs.com/" class="logo-link" id="sitelogo">
-                                                                                <img src="/logo.png" alt="Silicon Labs" width="200"/>
-                                                                        </a>
-                                                                </div>
-                                                        </td>
-                                                </tr>
-                                        </table>
-                                        <hr>
-                                </div>
-                        </div>
-                </div>
-                <div class="row">
-                        <table>
-                                <tr>
-                                        <td>
-                                                <div class="logo">
-                                                        <img src="/BRD8022.png" alt="BRD" width="500"/>
-                                                </div>
-                                        </td>
-                                </tr>
-                        </table>
-                        <hr>
-                </div>
-                <div class="row">
-                        <h2 style="text-align:center;">Welcome to WFX!</h2>
-                        <hr>
-                </div>
-        </div>
+	<div class="column">
+		<div class="row">
+			<br>
+			<div class="column">
+				<div class="row">
+					<hr>
+					<table>
+						<tr>
+							<td>
+								<div class="logo">
+									<a href="//www.silabs.com/" class="logo-link" id="sitelogo">
+										<img src="/logo.png" alt="Silicon Labs" width="200"/>
+									</a>
+								</div>
+							</td>
+						</tr>
+					</table>
+					<hr>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<table>
+				<tr>
+					<td>
+						<div class="logo">
+							<img src="/BRD8022.png" alt="BRD" width="500"/>
+						</div>
+					</td>
+				</tr>
+			</table>
+			<hr>
+		</div>
+		<div class="row">
+			<h2 style="text-align:center;">Welcome to WFX!</h2>
+			<hr>
+		</div>
+	</div>
   </body>
 """)
 
@@ -80,115 +80,115 @@ print ("""<div class="column">""")
 print ("<h4>%s</h4>"        % (date))
 
 if 1:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>%s</h1></td>"  % "System information")
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    print ("<li>%s</li>"  % (model))
-    print ("<li>Kernel %s</li>"  % (kernel))
-    print ("<li>Hostname: %s</li>"  % (hostname))
-    print ("<li>EXP board:%s</li>"  % (board))
-    print ("<li>Bus: %s</li>"  % (bus))
-    print ("<li>Driver:   %s</li>"  % (driver))
-    print ("<li>Firmware: %s</li>"  % (firmware))
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>%s</h1></td>"  % "System information")
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	print ("<li>%s</li>"  % (model))
+	print ("<li>Kernel %s</li>"  % (kernel))
+	print ("<li>Hostname: %s</li>"  % (hostname))
+	print ("<li>EXP board:%s</li>"  % (board))
+	print ("<li>Bus: %s</li>"  % (bus))
+	print ("<li>Driver:   %s</li>"  % (driver))
+	print ("<li>Firmware: %s</li>"  % (firmware))
 
-        print ("</h3>")
-        print ("</ul>")
-        print ("</td>")
-        print ("</tr>")
-        print ("</table>")
-
-if 1:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>%s</h1></td>"  % "IP addresses")
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    print ("<li>LAN  %s</li>"  % (bash_res("../info.sh ip eth")))
-    print ("<li>WLAN %s</li>"  % (bash_res("../info.sh ip wlan")))
-    print ("</h3>")
-    print ("</ul>")
-    print ("</td>")
-    print ("</tr>")
-    print ("</table>")
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
 
 if 1:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>%s</h1></td>"  % "MAC addresses")
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    print ("<li>LAN  %s</li>"  % (bash_res("../info.sh mac eth")))
-    print ("<li>WLAN %s</li>"  % (bash_res("../info.sh mac wlan")))
-    print ("</h3>")
-    print ("</ul>")
-    print ("</td>")
-    print ("</tr>")
-    print ("</table>")
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>%s</h1></td>"  % "IP addresses")
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	print ("<li>LAN  %s</li>"  % (bash_res("../info.sh ip eth")))
+	print ("<li>WLAN %s</li>"  % (bash_res("../info.sh ip wlan")))
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
+
+if 1:
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>%s</h1></td>"  % "MAC addresses")
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	print ("<li>LAN  %s</li>"  % (bash_res("../info.sh mac eth")))
+	print ("<li>WLAN %s</li>"  % (bash_res("../info.sh mac wlan")))
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
 
 if "ap" in mode:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>  %s mode</h1></td>"  % (mode))
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    print ("<li>%s</li>"  % (bash_res("../info.sh ap ssid")))
-    print ("<li>%s</li>"  % (bash_res("../info.sh ap mgmt")))
-    print ("<li>%s</li>"  % (bash_res("../info.sh ap wpa=")))
-    print ("<li>%s</li>"  % (bash_res("../info.sh ap channel")))
-    print ("</h3>")
-    print ("</ul>")
-    print ("</td>")
-    print ("</tr>")
-    print ("</table>")
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>  %s mode</h1></td>"  % (mode))
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	print ("<li>%s</li>"  % (bash_res("../info.sh ap ssid")))
+	print ("<li>%s</li>"  % (bash_res("../info.sh ap mgmt")))
+	print ("<li>%s</li>"  % (bash_res("../info.sh ap wpa=")))
+	print ("<li>%s</li>"  % (bash_res("../info.sh ap channel")))
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
 
 if "station" in mode:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>  %s mode</h1></td>"  % (mode))
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    print ("<li>%s</li>"  % (bash_res("../info.sh station country")))
-    print ("<li>%s</li>"  % (bash_res("../info.sh station wpa_state")))
-    print ("<li>%s</li>"  % (bash_res("../info.sh station ^address")))
-    print ("</h3>")
-    print ("</ul>")
-    print ("<ul>")
-    print ("<h2>Visible Access Points</h2>")
-    print ("<h3>")
-    scan_res = bash_res("../info.sh station scan")
-    for line in scan_res.splitlines():
-        print ("<li>%s\n</li>"       % (line))
-        print ("</h3>")
-        print ("</ul>")
-        print ("</td>")
-        print ("</tr>")
-        print ("</table>")
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>  %s mode</h1></td>"  % (mode))
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	print ("<li>%s</li>"  % (bash_res("../info.sh station country")))
+	print ("<li>%s</li>"  % (bash_res("../info.sh station wpa_state")))
+	print ("<li>%s</li>"  % (bash_res("../info.sh station ^address")))
+	print ("</h3>")
+	print ("</ul>")
+	print ("<ul>")
+	print ("<h2>Visible Access Points</h2>")
+	print ("<h3>")
+	scan_res = bash_res("../info.sh station scan")
+	for line in scan_res.splitlines():
+		print ("<li>%s\n</li>"       % (line))
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
 
 if 1:
-    print ("""<table border="1">""")
-    print ("<tr>")
-    print ("<td><h1>%s</h1></td>"  % "WFX Checks")
-    print ("<td>")
-    print ("<ul>")
-    print ("<h3>")
-    for line in checks.splitlines():
-        print ("<li>%s\n</li>"       % (line))
-        print ("</h3>")
-        print ("</ul>")
-        print ("</td>")
-        print ("</tr>")
-        print ("</table>")
+	print ("""<table border="1">""")
+	print ("<tr>")
+	print ("<td><h1>%s</h1></td>"  % "WFX Checks")
+	print ("<td>")
+	print ("<ul>")
+	print ("<h3>")
+	for line in checks.splitlines():
+		print ("<li>%s\n</li>"       % (line))
+	print ("</h3>")
+	print ("</ul>")
+	print ("</td>")
+	print ("</tr>")
+	print ("</table>")
 
 print ("""</div>""")
 
 print ("""
-          </body>
-        </html>
+	  </body>
+	</html>
 """)
