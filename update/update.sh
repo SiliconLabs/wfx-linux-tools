1240ffbb58c434c9c5a6e110771b60988e2a04c2
Xavier Langellier <xavier.langelier@silabs.com>
2019-01-16 15:03:33 +0000

    Update tools to 2.0

--- update/update.sh
+++ update/update.sh
@@ -13,7 +13,7 @@ fi
 
 set -x
 
-"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 0.26
+"/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" 2.0
 
 wfx_fetch
 wfx_driver_install --tag 2.0-public
