a355272cd5a00597533e6c9119291ad38cf9b06b
Xavier Langellier <xavier.langelier@silabs.com>
2019-02-06 10:56:44 +0000

    Disable trace in install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ set -e
 [ $(id -u) != 0 ] && echo "Please run this script as root (running 'sudo $0' should work)" && exit 1 || true
 ! [ -e install.sh ] && echo "This script must be run from wfx-linux-tools" && exit 1 || true
 
-set -x
-
 rm -f /usr/local/bin/wfx_*
 rm -f /usr/local/bin/pds_compress
 
