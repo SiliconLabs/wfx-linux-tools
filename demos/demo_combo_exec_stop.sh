ddebc57d30042fb01d83587ecb811174fddb8417
Xavier Langellier <xavier.langelier@silabs.com>
2019-09-27 20:00:27 +0100

    demo_combo_exec_stop.sh: update for IP forward disabled

--- demos/demo_combo_exec_stop.sh
+++ demos/demo_combo_exec_stop.sh
@@ -9,7 +9,3 @@ set -e
 
 # Remove second interface
 iw dev wlan1 del
-
-# Disable traffic routing from AP to STA network
-echo 0 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -D POSTROUTING -o wlan0 -j MASQUERADE
