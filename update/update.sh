18a20d867af4c2e970cb5ef0a7c859b949ef1765
Xavier Langellier <xavier.langelier@silabs.com>
2019-11-28 16:13:08 +0100

    update.sh: fix driver version

--- update/update.sh
+++ update/update.sh
@@ -15,6 +15,6 @@ set -x
 
 "/home/pi/siliconlabs/wfx-linux-tools/update/wfx_tools_install" origin/SD3
 
-wfx_driver_install --version 2.2.5-publicw
+wfx_driver_install --version 2.2.5-public
 fx_firmware_install --version FW3.3.0
 wfx_pds_install --auto
